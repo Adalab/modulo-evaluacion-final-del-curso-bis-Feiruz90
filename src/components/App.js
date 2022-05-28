f93e74dc991212e1c2b7ea35f54ec04c66c078a0
Feiruz Khouri <feiruz25@hotmail.com>
2022-05-28 18:37:31 +0200

    done/ part

--- src/components/App.js
+++ src/components/App.js
@@ -6,10 +6,13 @@ import ListRecipe from "./ListRecipe";
 import Filter from "./Filter";
 import DetailRecipe from "./DetailRecipe";
 import styles from "../styles/App.scss";
+import localStorage from "../services/localStorage";
 
 /*useState es una función que retorna un array con dos datos. El primero se va a guardar en la primera constante, es decir, en dataRecipes. El segundo valor del array devuelto por useState se va a guardar en la segunda constante declarada, es decir, en seTdataRecipes. De esta forma guardo los datos en un array. */
 function App() {
-  const [dataRecipes, setDataRecipes] = useState([]);
+  const [dataRecipes, setDataRecipes] = useState(
+    localStorage.get("recipes", [])
+  );
   const [filterCuisine, setFilterCuisine] = useState(""); // ESTE PARETESIS ES UN STRING VACIO , NO HAY INFORMACION AL PRINCIPIO
   console.log(filterCuisine);
   /* con el useEffect (está importando en la linea 1) :Cada vez que se arranca la página hay que obtener las recetas llamando a esta API:*/
@@ -23,6 +26,9 @@ function App() {
   }, []);
   /* despues del useeffect he creado el componenente ListRecipe*/
   // esta funcion de handlefiltercusine sirve para llamar a la funcion q actualiza la variable de estado ( FILTERCUISINE)
+  useEffect(() => {
+    localStorage.set("recipes", dataRecipes);
+  }, [dataRecipes]);
   const handleFilterCuisine = (value) => {
     setFilterCuisine(value);
   };
